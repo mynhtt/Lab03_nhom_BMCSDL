2bfc99ab66d29c11fbc4c635bf48a48eea833d2a
huy9633 <lamchanhuy9633@gmail.com>
2023-03-28 22:54:23 +0700

    update Login
    update Login

--- Lab03_nhom/DangNhap.Designer.cs
+++ Lab03_nhom/DangNhap.Designer.cs
@@ -32,12 +32,12 @@
             this.iconButton1 = new FontAwesome.Sharp.IconButton();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonHuy = new System.Windows.Forms.Button();
+            this.buttonDangNhap = new System.Windows.Forms.Button();
             this.textBoxMatKhau = new System.Windows.Forms.TextBox();
             this.textBoxTaiKhoan = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.buttonDangNhap = new System.Windows.Forms.Button();
-            this.buttonHuy = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -49,32 +49,37 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 79);
+            this.panel1.Size = new System.Drawing.Size(622, 52);
             this.panel1.TabIndex = 0;
             // 
             // iconButton1
             // 
+            this.iconButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.iconButton1.FlatAppearance.BorderSize = 0;
             this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
             this.iconButton1.IconColor = System.Drawing.Color.White;
             this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton1.IconSize = 54;
-            this.iconButton1.Location = new System.Drawing.Point(703, 17);
+            this.iconButton1.Location = new System.Drawing.Point(547, 11);
+            this.iconButton1.Margin = new System.Windows.Forms.Padding(2);
             this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Size = new System.Drawing.Size(94, 50);
+            this.iconButton1.Size = new System.Drawing.Size(73, 33);
             this.iconButton1.TabIndex = 1;
             this.iconButton1.UseVisualStyleBackColor = true;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(263, 0);
+            this.label1.Location = new System.Drawing.Point(205, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(270, 67);
+            this.label1.Size = new System.Drawing.Size(219, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "Đăng nhập";
             // 
@@ -87,32 +92,59 @@
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 79);
+            this.panel2.Location = new System.Drawing.Point(0, 52);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 371);
+            this.panel2.Size = new System.Drawing.Size(622, 241);
             this.panel2.TabIndex = 1;
+            // 
+            // buttonHuy
+            // 
+            this.buttonHuy.Location = new System.Drawing.Point(355, 181);
+            this.buttonHuy.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonHuy.Name = "buttonHuy";
+            this.buttonHuy.Size = new System.Drawing.Size(99, 25);
+            this.buttonHuy.TabIndex = 5;
+            this.buttonHuy.Text = "Hủy";
+            this.buttonHuy.UseVisualStyleBackColor = true;
+            this.buttonHuy.Click += new System.EventHandler(this.buttonHuy_Click);
+            // 
+            // buttonDangNhap
+            // 
+            this.buttonDangNhap.Location = new System.Drawing.Point(163, 181);
+            this.buttonDangNhap.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDangNhap.Name = "buttonDangNhap";
+            this.buttonDangNhap.Size = new System.Drawing.Size(99, 25);
+            this.buttonDangNhap.TabIndex = 4;
+            this.buttonDangNhap.Text = "Đăng nhập";
+            this.buttonDangNhap.UseVisualStyleBackColor = true;
+            this.buttonDangNhap.Click += new System.EventHandler(this.buttonDangNhap_Click);
             // 
             // textBoxMatKhau
             // 
-            this.textBoxMatKhau.Location = new System.Drawing.Point(358, 184);
+            this.textBoxMatKhau.Location = new System.Drawing.Point(278, 120);
+            this.textBoxMatKhau.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxMatKhau.Name = "textBoxMatKhau";
-            this.textBoxMatKhau.Size = new System.Drawing.Size(226, 30);
+            this.textBoxMatKhau.Size = new System.Drawing.Size(177, 23);
             this.textBoxMatKhau.TabIndex = 3;
+            this.textBoxMatKhau.UseSystemPasswordChar = true;
             // 
             // textBoxTaiKhoan
             // 
-            this.textBoxTaiKhoan.Location = new System.Drawing.Point(358, 104);
+            this.textBoxTaiKhoan.Location = new System.Drawing.Point(278, 68);
+            this.textBoxTaiKhoan.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxTaiKhoan.Name = "textBoxTaiKhoan";
-            this.textBoxTaiKhoan.Size = new System.Drawing.Size(226, 30);
+            this.textBoxTaiKhoan.Size = new System.Drawing.Size(177, 23);
             this.textBoxTaiKhoan.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(216, 180);
+            this.label3.Location = new System.Drawing.Point(168, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 32);
+            this.label3.Size = new System.Drawing.Size(91, 25);
             this.label3.TabIndex = 1;
             this.label3.Text = "Mật khẩu";
             // 
@@ -120,41 +152,24 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(216, 100);
+            this.label2.Location = new System.Drawing.Point(168, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 32);
+            this.label2.Size = new System.Drawing.Size(96, 25);
             this.label2.TabIndex = 0;
             this.label2.Text = "Tài khoản:";
             // 
-            // buttonDangNhap
-            // 
-            this.buttonDangNhap.Location = new System.Drawing.Point(209, 277);
-            this.buttonDangNhap.Name = "buttonDangNhap";
-            this.buttonDangNhap.Size = new System.Drawing.Size(127, 38);
-            this.buttonDangNhap.TabIndex = 4;
-            this.buttonDangNhap.Text = "Đăng nhập";
-            this.buttonDangNhap.UseVisualStyleBackColor = true;
-            this.buttonDangNhap.Click += new System.EventHandler(this.buttonDangNhap_Click);
-            // 
-            // buttonHuy
-            // 
-            this.buttonHuy.Location = new System.Drawing.Point(457, 277);
-            this.buttonHuy.Name = "buttonHuy";
-            this.buttonHuy.Size = new System.Drawing.Size(127, 38);
-            this.buttonHuy.TabIndex = 5;
-            this.buttonHuy.Text = "Hủy";
-            this.buttonHuy.UseVisualStyleBackColor = true;
-            this.buttonHuy.Click += new System.EventHandler(this.buttonHuy_Click);
-            // 
             // DangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(622, 293);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DangNhap";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
--- Lab03_nhom/DangNhap.cs
+++ Lab03_nhom/DangNhap.cs
@@ -5,30 +5,90 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Lab03_nhom
 {
     public partial class DangNhap : Form
     {
+        string strConn = @"Data Source=LAPTOP-5M61K3MD\SQLEXPRESS;Initial Catalog=QLSV_BOBA;Integrated Security=True;MultipleActiveResultSets=True";
+        SqlConnection sqlconn = null;
         public DangNhap()
         {
             InitializeComponent();
         }
 
-        private bool kiemTraDangNhap()
-        {
-            return false;
-        }
         private void buttonDangNhap_Click(object sender, EventArgs e)
+
         {
-            if (kiemTraDangNhap())
+            var TaiKhoan = textBoxTaiKhoan.Text.Trim();
+            var MatKhau = textBoxMatKhau.Text.Trim();
+
+            if (sqlconn == null)
             {
-                Form1 f1 = new Form1();
-                f1.Show();
+                sqlconn = new SqlConnection(strConn);
+            }
+
+            SqlCommand cmd_NV = new SqlCommand("SP_CHECK_NV", sqlconn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd_NV.Parameters.Add("@TENDN", SqlDbType.VarChar).Value = TaiKhoan;
+            cmd_NV.Parameters.Add("@MATKHAU", SqlDbType.VarChar).Value = MatKhau;
+
+            SqlCommand cmd_SV = new SqlCommand("SP_CHECK_SV", sqlconn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd_SV.Parameters.Add("@TENDN", SqlDbType.VarChar).Value = TaiKhoan;
+            cmd_SV.Parameters.Add("@MATKHAU", SqlDbType.VarChar).Value = MatKhau;
+            if (sqlconn.State == ConnectionState.Closed)
+            {
+                sqlconn.Open();
+            }
+            using SqlDataReader kq_SV = cmd_SV.ExecuteReader();
+
+            if (kq_SV.Read())
+            {
+                MessageBox.Show("Đăng nhập thành công tài khoản sinh viên " + kq_SV[0], "Thông Báo");
+            }
+            else
+            {
+                sqlconn.Close();
+
+                if (sqlconn.State == ConnectionState.Closed)
+                {
+                    sqlconn.Open();
+                }
+                using SqlDataReader kq_NV = cmd_NV.ExecuteReader();
+                if (kq_NV.Read())
+                {
+                    string MANV = kq_NV[1].ToString();
+
+                    MessageBox.Show("Đăng nhập thành công tài khoản nhân viên " + kq_NV[0], "Thông Báo");
+                    Form1 frmManagerStudent = new Form1();
+                    //= new frmManagerClass(MANV, MatKhau); 
+                    this.Hide();
+                    frmManagerStudent.ShowDialog();
+                    frmManagerStudent = null;
+                    this.Show();
+                    textBoxTaiKhoan.Text = "";
+                    textBoxMatKhau.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Tên đăng nhập và mật khẩu không hợp lệ", "Thông Báo");
+                }
+                sqlconn.Close();
             }
         }
 
         private void buttonHuy_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
         {
             this.Close();
         }
